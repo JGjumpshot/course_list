17b3adb42c033520fb11f6534c0295e75624b9b6
Jackson Gunther <jackson.gunther@yahoo.com>
2021-10-06 18:58:13 -0600

    fixed find and insert

--- courselist.py
+++ courselist.py
@@ -9,15 +9,15 @@ class CourseList:
         new_course = course
         if self.head is None:
             self.head = new_course
-            self.size += 1
         new_course.next_node = self.head
         self.head = new_course
         self.size += 1
     def find(self, element):
         current = self.head
         while current is not None:
+            print(current)
             if current.course_name == element:
-                return self.size
+                return current
             current = current.next_node
         return -1
     def __iter__(self):
